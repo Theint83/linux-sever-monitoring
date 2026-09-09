da557381fd4c2ea8786ee2bfa2e9f935a36fa47b
honeykyaw <honeykyawkyaw186@gmail.com>
2026-09-10 03:46:38 +0630

    Fix CORS and dynamic log

--- app.py
+++ app.py
@@ -10,51 +10,59 @@ CORS(app)
 
 boot_time = psutil.boot_time()
 logs_list=[]
-log_id_counter=1	
+log_id_counter=1
+	
 @app.route('/api/monitoring-data')
 def get_monitoring_data():
-	global log_id_counter,logs_list
-	uptime_seconds = int(time.time() - boot_time)
-	days,remainder=divmod(uptime_seconds,86400)
-	hours,remainder=divmod(uptime_seconds,3600)
-	minutes,seconds=divmod(remainder,60)
-	uptime_str = f"{days}d {hours}h {minutes}m"
+    global log_id_counter,logs_list
+    uptime_seconds = int(time.time() - boot_time)
+    days,remainder=divmod(uptime_seconds,86400)
+    hours,remainder=divmod(uptime_seconds,3600)
+    minutes,seconds=divmod(remainder,60)
+    uptime_str = f"{days}d {hours}h {minutes}m"
 	
 	
-	disk=psutil.disk_usage('/')
-	cpu=psutil.cpu_percent(interval=0.5)
-	ram=psutil.virtual_memory().percent
+    disk=psutil.disk_usage('/')
+    cpu=psutil.cpu_percent(interval=0.5)
+    ram=psutil.virtual_memory().percent
 	
-	alerts=0
-	if cpu>70: alerts +=1
-	if ram>80:alerts += 1
+    alerts=0
+    if cpu>60 or ram>75:
+        log_level = "Warning"
+        alerts +=1
+    elif cpu > 85 or ram>90:
+        log_level = "Critical"
+        alerts += 2
+    else:
+        log_level = "Info"
 	
-	now_str = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-	new_logs={	
-		"id":log_id_counter,
-		"timestamp":now_str,
-		"message": f"System Check: CPU ({cpu}%),RAM ({ram}%)",
-		"Level": "Warning" if (cpu > 70 or ram >80) else "INFO"
-	}
-	logs_list=[]
-	log_id_counter = 1
+    now_str =datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    new_logs={	
+        "id":log_id_counter,
+        "timestamp":now_str,
+        "message": f"System Check: CPU ({cpu}%),RAM ({ram}%)",
+        "level": "Warning" if (cpu > 70 or ram >80) else "INFO"
+        }
 
-	if len(logs_list) > 10:
-        	logs_list = logs_list[:10]
+    logs_list.append(new_logs)
+    log_id_counter += 1
 
-	return jsonify({
-	"server_status": "Running",
-	"ip_address": socket.gethostbyname(socket.gethostname()),
-	"uptime": uptime_str,
-	"security_warnings": alerts,
-	"cpu_usage": cpu,
-	"ram_usage": ram,
-	"disk_used": round(disk.used /(1024**3),2),
-	"disk_free": round(disk.free / (1024**3),2),
-	"logs": logs_list,
-	"network_stats":{
-		"net_status": "ONLINE"}
-	})
+    if len(logs_list) > 10:
+        logs_list = logs_list[-10:]
+
+    return jsonify({
+        "server_status": "Running",
+        "ip_address": socket.gethostbyname(socket.gethostname()),
+        "uptime": uptime_str,
+        "security_warnings": alerts,
+        "cpu_usage": cpu,
+        "ram_usage": ram,
+        "disk_used": round(disk.used /(1024**3),2),
+        "disk_free": round(disk.free / (1024**3),2),
+        "network_stats":{
+        "net_status": "ONLINE"},
+        "logs": logs_list
+    })
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5000,debug=True)
